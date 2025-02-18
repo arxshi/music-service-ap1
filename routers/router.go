df68ecaa0cba2cd80ea14aab6ca8d1b4f2dbafa3
snkmqn <160516534+snkmqn@users.noreply.github.com>
2025-02-18 14:20:10 +0500

    Add files via upload
    updated routes

--- routers/router.go
+++ routers/router.go
@@ -10,6 +10,7 @@ func InitRouter(router *gin.Engine, db *mongo.Database) {
 	router.POST("/auth/register", func(c *gin.Context) { controllers.RegisterUser(c, db.Collection("users")) })
 	router.POST("/auth/login", func(c *gin.Context) { controllers.Login(c, db.Collection("users")) })
 	router.GET("/users/:user_id", func(c *gin.Context) { controllers.GetUser(c, db.Collection("users")) })
+	router.GET("/users", func(c *gin.Context) { controllers.GetUser(c, db.Collection("users")) })
 	router.GET("/tracks", func(c *gin.Context) { controllers.GetTracks(c, db.Collection("tracks")) })
 	router.GET("/tracks/:track_id", func(c *gin.Context) { controllers.GetTrack(c, db.Collection("tracks")) })
 	router.DELETE("/tracks/:track_id", func(c *gin.Context) { controllers.DeleteTrack(c, db.Collection("tracks")) })
@@ -20,4 +21,6 @@ func InitRouter(router *gin.Engine, db *mongo.Database) {
 	router.DELETE("/playlists/:playlist_id/tracks/:track_id", func(c *gin.Context) { controllers.RemoveTrackFromPlaylist(c, db.Collection("playlists")) })
 	router.POST("/reviews", func(c *gin.Context) { controllers.CreateReview(c, db.Collection("reviews")) })
 	router.GET("/reviews", func(c *gin.Context) { controllers.GetReviews(c, db.Collection("reviews")) })
+	router.POST("/upload", func(c *gin.Context) { controllers.UploadTrack(c, db.Collection("tracks")) })
+	router.GET("/files/:filename", controllers.GetTrackFile)
 }
